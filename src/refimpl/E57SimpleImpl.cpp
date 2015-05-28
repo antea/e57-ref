da15b0e59deb52ab6e90610e154c9510ff675f29
Federico Russo <russo.federico@anteash.com>
2015-05-28 12:09:37 +0200

    Builds the Simple interface.

--- src/refimpl/E57SimpleImpl.cpp
+++ src/refimpl/E57SimpleImpl.cpp
@@ -74,6 +74,14 @@
 #  include <boost/uuid/uuid.hpp>
 #  include <boost/uuid/uuid_generators.hpp>
 #  include <boost/uuid/uuid_io.hpp>
+#elif defined(__APPLE__)
+#  define _LARGEFILE64_SOURCE
+#  define __LARGE64_FILES
+#  include <sys/types.h>
+#  include <unistd.h>
+#  include <boost/uuid/uuid.hpp>
+#  include <boost/uuid/uuid_generators.hpp>
+#  include <boost/uuid/uuid_io.hpp>
 #else
 #  error "no supported OS platform defined"
 #endif
